d676dbc4b92f9c1c04ed387c978364ebf64d04aa
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-02-23 22:10:47 +0800

    add custom validation for booking form date field and guest field

--- booking/forms.py
+++ booking/forms.py
@@ -1,7 +1,6 @@
 import datetime
 from .models import Booking, available_times
 from django import forms
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your forms here.
@@ -21,7 +20,25 @@ class BookingForm(forms.ModelForm):
             'date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.Select(choices=available_times),
         }
-        validators = {
-            'number_of_people': [MinValueValidator(1), MaxValueValidator(6)],
-            'date': [MinValueValidator(datetime.date.today)]
-        }
+    
+    def clean_date(self):
+        """
+        Validation for the date field
+        """
+        date = self.cleaned_data['date']
+        
+        if date < datetime.date.today():
+            raise forms.ValidationError("Booking date cannot be in the past.")
+        return date
+
+    def clean_number_of_people(self):
+        """
+        Validation for the number_of_people field
+        """
+        number_of_people = self.cleaned_data['number_of_people']
+
+        if number_of_people < 1:
+            raise forms.ValidationError("Number of people must be at least 1.")
+        elif number_of_people > 6:
+            raise forms.ValidationError("Number of people cannot exceed 6.")
+        return number_of_people
