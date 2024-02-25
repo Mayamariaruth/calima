799a2b128c9f339bd466ff8a761d4f746ef807da
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-02-25 23:14:48 +0800

    set up menu app with html template

--- calima/urls.py
+++ calima/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import Group
 from django.urls import path, include
+from menu.views import menu_page
 
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('menu/', menu_page, name="menu_page"),
     path('', include('booking.urls'), name='booking_urls'),
     path('', include('home.urls'), name='home_urls'),
     path('', include('profile_account.urls'), name='profile_urls'),
